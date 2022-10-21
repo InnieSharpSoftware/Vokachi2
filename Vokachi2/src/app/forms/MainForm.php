f2b07dc797dd5d606c0d296263dd72099f767e2a
InnieSharpSoftware <116297277+InnieSharpSoftware@users.noreply.github.com>
2022-10-21 20:44:13 +0300

    Default

---
+++ Vokachi2/src/app/forms/MainForm.php
@@ -0,0 +1,34 @@
+<?php
+namespace app\forms;
+
+use std, gui, framework, app;
+
+
+class MainForm extends AbstractForm
+{
+
+    /**
+     * @event button.action 
+     */
+    function doButtonAction(UXEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event buttonAlt.action 
+     */
+    function doButtonAltAction(UXEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event button3.action 
+     */
+    function doButton3Action(UXEvent $e = null)
+    {    
+        
+    }
+
+}
---
+++ Vokachi2/src/app/forms/objs.php
@@ -0,0 +1,18 @@
+<?php
+namespace app\forms;
+
+use std, gui, framework, app;
+
+
+class objs extends AbstractForm
+{
+
+    /**
+     * @event rect.click-Right 
+     */
+    function doRectClickRight(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+}
